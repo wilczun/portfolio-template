6b56b9d422ee379f33421c02ae7e8b4aa2c6d732
Paul <56404443+wilczun@users.noreply.github.com>
2020-01-03 18:51:30 +0100

    mobile friendly content added

--- portfolio-psd-html/js/script.js
+++ portfolio-psd-html/js/script.js
@@ -5,3 +5,18 @@ $(window).scroll(function () {
     $('#dynamic').removeClass('bg-change');
   }
 });
+
+const nav = document.querySelector('.nav-links');
+const burger = document.querySelector('.burger')
+const links = document.querySelectorAll('a')
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav-open");
+  burger.classList.toggle("toggle")
+});
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    nav.classList.toggle("nav-open")
+    burger.classList.toggle("toggle")
+  });
+});
